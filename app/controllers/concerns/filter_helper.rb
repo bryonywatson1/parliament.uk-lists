3d03d2b9f2748840ec88e03b29d699c5f19f1772
Bryony Watson <watsonb@parliament.uk>
2017-11-13 17:27:13 +0000

    not sorting letters

--- app/controllers/concerns/filter_helper.rb
+++ app/controllers/concerns/filter_helper.rb
@@ -9,31 +9,26 @@ module FilterHelper
 
 
   def self.filter(request, *types)
-    types_to_filter = []
-    types.each do |type|
-      if type == 'ordnance'
-        types_to_filter << 'http://data.ordnancesurvey.co.uk/ontology/admingeo/EuropeanRegion'
-      else
-      types_to_filter << Parliament::Utils::Helpers::RequestHelper.namespace_uri_schema_path(type)
-      end
-    end
+    types_to_filter = self.filter_types(*types)
     Parliament::Utils::Helpers::RequestHelper.filter_response_data(
-      request, *types_to_filter)
+      request, *types_to_filter
+    )
   end
 
   def self.filter_letters(request, *types)
-    types_to_filter = []
-    types.each do |type|
-      if type == 'ordnance'
-        types_to_filter << 'http://data.ordnancesurvey.co.uk/ontology/admingeo/EuropeanRegion'
-      else
-      types_to_filter << Parliament::Utils::Helpers::RequestHelper.namespace_uri_schema_path(type)
-      end
-    end
+    types_to_filter = self.filter_types(*types)
     Parliament::Utils::Helpers::RequestHelper.filter_response_data(
       request, *types_to_filter,
       ::Grom::Node::BLANK
     )
+  end
+
+  def self.filter_types(*types)
+    types_to_filter = []
+    types.each do |type|
+      type == 'ordnance' ? types_to_filter << 'http://data.ordnancesurvey.co.uk/ontology/admingeo/EuropeanRegion' : types_to_filter << Parliament::Utils::Helpers::RequestHelper.namespace_uri_schema_path(type)
+    end
+    types_to_filter
   end
 
 end
